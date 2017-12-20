75298908ef903103f244e218c7e53b2e80f1593d
Alex Michel <amishel@gmail.com>
2017-12-20 13:56:51 +0200

    Initial

---
+++ DotNetConsoleClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace DotNetConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var socket = new DealerSocket())
+            {
+                socket.Connect("tcp://127.0.0.1:5560");
+
+                for (int i = 0; i < 10000; i++)
+                {
+                    socket.SendFrame(Encoding.UTF8.GetBytes("kkk"));
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
